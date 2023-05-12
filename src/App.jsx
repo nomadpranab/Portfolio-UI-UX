6b2c0cb4866b00fee7102ac3a527914957dee117
PRANAB PATRA <pranabpatra520@gmail.com>
2023-05-12 16:08:51 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,30 @@ import Experience from './components/Experience/Experience'
 import Portfolio from './components/Portfolio/Portfolio'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import { useState } from 'react'
+import loader from './assets/spinnergif.svg'
+
 
 
 function App() {
+
+  const[loading,setloading]=useState(true);
+
+  setTimeout(()=>{
+      setloading(false);
+  },5000);
+  
+  if(loading===true)
+  {
+      return (
+          <div className=" loader container text-center">
+              <img id="loaderimage" src={loader} alt='spinner'/>
+          </div>
+        )
+  }
+  else{
   return (
     <>
-  
     <Header/>
     <Navbar/>
     <About/>
@@ -19,9 +37,9 @@ function App() {
     <Portfolio/>
     <Contact/>
     <Footer/>
-
     </>
   )
+  }
 }
 
 export default App
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,25 +2,34 @@ import React from 'react'
 import CTA from './CTA'
 import Me from '../../assets/me.png'
 import HeaderSocial from './HeaderSocial'
+import { useState } from 'react'
+import loader from '../../assets/spinnergif.svg'
 function Header() {
-  return (
-    <header>
-        <div className="container header__container">
-            <h5>Hello I'm</h5>
-            <h1>Pranab Patra</h1>
-            <h5 className='text-light'>Fullstack Developer</h5>
-            <CTA/>
-            <HeaderSocial/>
-            <div className='me'>
-                <img src={Me} alt=''/>
-             </div>
-             <a href="#contact" className='scroll_down'>Scroll Down</a>
-            
-             </div>
 
-             
-    </header>
-  )
-}
+
+
+      return (
+        <header>
+            <div className="container header__container">
+                <h5>Hello I'm</h5>
+                <h1>Pranab Patra</h1>
+                <h5 className='text-light'>Fullstack Developer</h5>
+                <CTA/>
+                <HeaderSocial/>
+                <div className='me'>
+                    <img src={Me} alt=''/>
+                 </div>
+                 <a href="#contact" className='scroll_down'>Scroll Down</a>
+                
+                 </div>
+    
+                 
+        </header>
+      )
+        
+    }
+
+  
+
 
 export default Header
