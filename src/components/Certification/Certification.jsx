dc33f2e55abd612ba5a56ae20e2f3fdc4672625b
PRANAB PATRA <pranabpatra520@gmail.com>
2023-05-17 16:08:12 +0530

    done

--- src/components/Certification/Certification.jsx
+++ src/components/Certification/Certification.jsx
@@ -6,16 +6,16 @@ function Certification() {
     <section>
       <h2 className='text-center'>My Certifications</h2>
       <div className="d-flex flex-wrap justify-content-evenly">
-        <div className='p-2' id='certficatediv' style={{width:"18rem",height:"30rem"}}>
-          <img src={require('../../assets/me11.png')} alt=''/>
+        <div className='p-2' id='certficatediv' style={{width:"20rem",height:"30rem"}}>
+          <img src={require('../../assets/Certificate1.png')} alt=''/>
         </div>
 
-        <div className='p-2' id='certficatediv'  style={{width:"18rem",height:"30rem"}}>
-          <img src={require('../../assets/me11.png')} alt=''/>
+        <div className='p-2' id='certficatediv'  style={{width:"20rem",height:"30rem"}}>
+          <img src={require('../../assets/Certificate2.png')} alt=''/>
         </div>
 
-        <div className='p-2' id='certficatediv' style={{width:"18rem",height:"30rem"}}>
-          <img src={require('../../assets/me11.png')} alt=''/>
+        <div className='p-2' id='certficatediv' style={{width:"20rem",height:"30rem"}}>
+          <img src={require('../../assets/Certificate3.png')} alt=''/>
         </div>
 
 
